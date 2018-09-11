89a781b416ce2dc8d4f6ceb2602a90d3c6514656
hirany <34570860+hirany@users.noreply.github.com>
2018-09-11 16:35:42 +0900

    Add files via upload

--- main.go
+++ main.go
@@ -21,7 +21,10 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.templ.Execute(w, nil)
 }
 func main() {
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "ht.html"})
+	http.Handle("/room", r)
+	go r.run()
 	if err := http.ListenAndServe(":8000", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
