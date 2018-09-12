16c2705bdfaf7ffd06ea502c5233bfd05fc17611
hirany <34570860+hirany@users.noreply.github.com>
2018-09-12 15:05:20 +0900

    Add files via upload

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
@@ -18,13 +19,16 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 func main() {
+	var addr = flag.String("addr", ":8000", "アプリケーション")
+	flag.Parse()
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "ht.html"})
 	http.Handle("/room", r)
 	go r.run()
+	log.Println("Webサーバを開始します。ポート: ", *addr)
 	if err := http.ListenAndServe(":8000", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
